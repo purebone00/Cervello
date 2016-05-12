62c24ddd40caa1dc3ffa0480cd5ab98e2d2892af
Jeff Nguyen <jeffnguyen91@hotmail.com>
2016-05-12 12:12:06 -0700

    options menu

--- js/script.js
+++ js/script.js
@@ -27,3 +27,26 @@ $(document).keydown(function(e) {
         alert('creators \n Albert \"Purebone00\" Chen \n Kevin \"Zireael\" Fong \n Jeff \"HitAndQuit007\" Nguyen \n Nitori \"Pomelo\" Nyamekye \n Matt \"peg631\" Lin ');
         }
 });
+
+var timeInSecs;
+var ticker;
+
+function startTimer(secs){
+    timeInSecs = parseInt(secs)-1;
+    ticker = setInterval("tick()",1000);   // every second
+}
+
+function tick() {
+    var secs = timeInSecs;
+    if (secs>0) {
+        timeInSecs--;
+    }
+    else {
+        clearInterval(ticker); // stop counting at zero
+        startTimer(10);  // remove forward slashes in front of startTimer to repeat if required
+    }
+
+    document.getElementById("clock").innerHTML = secs;
+}
+
+startTimer(10);  // 10 seconds 
